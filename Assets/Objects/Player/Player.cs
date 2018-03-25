5cd19ede195a9f95863df211b30acf0bf0aeda6d
MininMobile <miningamer@gmail.com>
2018-03-25 16:22:54 +0100

    Basic Movement

--- Assets/Objects/Player/Player.cs
+++ Assets/Objects/Player/Player.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float Speed = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    void Update () {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * Speed,
+            0,
+            0
+        );
 	}
 }
